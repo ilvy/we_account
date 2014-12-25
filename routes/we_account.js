df387c4804e696125fe8e6f8ac4c867f01e6fb8e
ilvy <478283225@qq.com>
2014-12-25 18:42:41 +0800

    resolve upload img path

--- routes/we_account.js
+++ routes/we_account.js
@@ -88,12 +88,16 @@ router.get("/publish",function(req,res){
 router.post("/upload",function(req,res){
     console.log(req);
     var form = new formidable.IncomingForm();
-    form.uploadDir = __dirname+'/public/images';
+    form.uploadDir = __dirname+'';
+    console.log(process.cwd()+" ******** "+__dirname);
     //form.uploadDir = __dirname + '/';
 
     form.on('file', function(field, file) {
         //rename the incoming file to the file's name
-        fs.rename(file.path, form.uploadDir + "/" + file.name);
+        console.log( "rename:"+path.normalize(process.cwd()+"/public/images/" + file.name));
+        fs.rename(file.path, path.normalize(process.cwd()+"/public/images/" + file.name),function(err){
+            console.log("newPath:"+file.path);
+        });
     })
         .on('error', function(err) {
             console.log("an error has occured with form upload");
